fc952dbc4a629cbd1704228e2f63ca07d2b7fa86
Randy Fay <randy@randyfay.com>
2025-01-14 11:47:46 -0700

    Add total montly into final number

--- scripts/combine-sponsorships.sh
+++ scripts/combine-sponsorships.sh
@@ -19,14 +19,16 @@ for f in ${inputs}; do
 done
 
 github_ddev_total_monthly=$(jq -r .github_ddev_sponsorships.total_monthly_sponsorship data/tmp/github-ddev-sponsorships.json)
+github_rfay_total_monthly=$(jq -r .github_rfay_sponsorships.total_monthly_sponsorship data/tmp/github-rfay-sponsorships.json)
 invoiced_total_monthly=$(jq -r .monthly_invoiced_sponsorships.total_monthly_sponsorship data/tmp/invoiced-sponsorships.json)
-invoiced_monthly_annual_equivalent=$(jq -r .annual_invoiced_sponsorships.monthly_equivalent_sponsorship data/tmp/invoiced-sponsorships.json)
+invoiced_annual_monthly_equivalent=$(jq -r .annual_invoiced_sponsorships.monthly_equivalent_sponsorship data/tmp/invoiced-sponsorships.json)
 paypal_total_monthly=$(jq -r .paypal_sponsorships data/tmp/paypal-sponsorships.json)
-
+total_monthly_average_income=$((github_ddev_total_monthly + github_rfay_total_monthly + invoiced_total_monthly + invoiced_annual_monthly_equivalent + paypal_total_monthly))
 
 ## Combine the files into one and add update time.
 jq -s --arg updated_datetime "$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
-  'reduce .[] as $item ({}; . * $item) + {updated_datetime: $updated_datetime}' \
+  --arg total_monthly_average_income ${total_monthly_average_income} \
+  'reduce .[] as $item ({}; . * $item) + {total_monthly_average_income: $total_monthly_average_income} + {updated_datetime: $updated_datetime}' \
   data/tmp/*.json > "${output_file}"
 
 echo "Combined JSONC written to $output_file" >&2
